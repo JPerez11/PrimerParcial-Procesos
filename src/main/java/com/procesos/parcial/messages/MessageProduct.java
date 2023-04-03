af45265597fb07563e97a8c99b9439e8e6cde146
Jhoan Sebastián Pérez Rueda <jhoansebas0301@gmail.com>
2023-04-02 19:05:21 -0500

    add package and classes

---
+++ src/main/java/com/procesos/parcial/messages/MessageProduct.java
@@ -0,0 +1,19 @@
+package com.procesos.parcial.messages;
+
+public enum MessageProduct {
+
+    PRODUCT_LIST("All products have been listed"),
+    PRODUCT_CREATED("The product has been create"),
+    PRODUCT_IMPORT("All products has been import"),
+    PRODUCT_UPDATED("The product has been update");
+
+    private final String message;
+
+    MessageProduct(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ src/main/java/com/procesos/parcial/model/Product.java
@@ -0,0 +1,32 @@
+package com.procesos.parcial.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "PRODUCT")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private double price;
+    @Column(length = 1000)
+    private String description;
+    private String category;
+    private String image;
+
+}
