83a081448317ca138186f8b046b1595b27d5f33e
Jhoan Sebastián Pérez Rueda <jhoansebas0301@gmail.com>
2023-04-19 21:20:27 -0500

    commenting the code

--- src/main/java/com/procesos/parcial/repository/IProductRepository.java
+++ src/main/java/com/procesos/parcial/repository/IProductRepository.java
@@ -3,7 +3,15 @@ package com.procesos.parcial.repository;
 import com.procesos.parcial.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Product repository interface
+ */
 public interface IProductRepository extends JpaRepository<Product, Long> {
 
+    /**
+     * Method to find a Product by ID.
+     * @param id Product identifier.
+     * @return The product found.
+     */
     Product findProductById(Long id);
 }
